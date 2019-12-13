508f55b48e20bc2a723a918c2d9599288d91f1fa
Ewelina Szmielew <ewelina@szmielew.pl>
2019-12-13 23:27:34 +0100

    merging data from different sources

--- ewardrobe/processing_data/merge_data_sources.py
+++ ewardrobe/processing_data/merge_data_sources.py
@@ -1,4 +1,5 @@
 import pandas
+import random
 from pathlib import Path
 from constance import COLUMNS
 
@@ -13,9 +14,13 @@ def merge_files(filenames: list) -> pandas.DataFrame:
     data = pandas.DataFrame(columns=COLUMNS)
     for file in filenames:
         data = data.append(pandas.read_csv(file, header=0))
+    data = data.drop(
+        ["mrp", "style_attributes", "total_sizes", "available_size"], axis=1
+    )
     return data
 
 
 if __name__ == "__main__":
     filenames = get_filenames()
     data = merge_files(filenames)
+    print(data.columns)
