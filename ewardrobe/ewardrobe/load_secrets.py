e40121c659caedd37c0cc2a39648a80019962b84
Ewelina Szmielew <ewelina@szmielew.pl>
2019-12-17 18:36:53 +0100

    Add basic models to ap structure

---
+++ ewardrobe/ewardrobe/load_secrets.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+
+# JSON based secrets module
+from django.core.exceptions import ImproperlyConfigured
+
+BASEDIR = Path(__file__).absolute().parent
+with open(BASEDIR.joinpath("secrets.json")) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets):
+    """ Trying to get setting key from secrets file if added"""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
--- ewardrobe/ewardrobe_app/models.py
+++ ewardrobe/ewardrobe_app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from enum import Enum
 
 # Create your models here.
+
+
+class Brand(models.Model):
+    brand_name = models.CharField()
+
+
+class Category(models.Model):
+    category = models.CharField()
+
+
+class Retailer(models.Model):
+    retailer = models.CharField()
+
+
+class Color(models.Model):
+    color = models.CharField()
+
+
+class Product(models.Model):
+    class SIZE(Enum):
+        XS = "XS"
+        S = "S"
+        M = "M"
+        L = "L"
+        XL = "XL"
+        XXL = "XXL"
+
+        @classmethod
+        def get_value(cls, member):
+            return cls[member].value
+
+    name = models.CharField()
+    price = models.DecimalField()
+    url = models.URLField()
+    description = models.CharField()
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    reviews_count = models.PositiveIntegerField()
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    retailer = models.ForeignKey(Retailer, on_delete=models.CASCADE)
+    size = models.CharField(
+        max_length=4, choices=[x.value for x in SIZE], default=SIZE.get_value("M"),
+    )
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["headline"]
