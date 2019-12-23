96936c51308b5a33a5d18d5f379f7a334229742e
Ewelina Szmielew <ewelina@szmielew.pl>
2019-12-23 22:59:56 +0100

    Add basket and user models

--- ewardrobe/ewardrobe_app/models.py
+++ ewardrobe/ewardrobe_app/models.py
@@ -1,42 +1,50 @@
 from django.db import models
 from enum import Enum
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
+class DateAddedMixin(models.Model):
+    date_added = models.DateField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
 class Brand(models.Model):
-    brand_name = models.CharField()
+    brand_name = models.CharField(max_length=25)
 
 
 class Category(models.Model):
-    category = models.CharField()
+    category = models.CharField(max_length=25)
 
 
 class Retailer(models.Model):
-    retailer = models.CharField()
+    retailer = models.CharField(max_length=25)
 
 
 class Color(models.Model):
-    color = models.CharField()
+    color = models.CharField(max_length=25)
 
 
-class Product(models.Model):
+class Product(DateAddedMixin, models.Model):
     class Size(Enum):
-        XS = "XS"
-        S = "S"
-        M = "M"
-        L = "L"
-        XL = "XL"
-        XXL = "XXL"
+        XS = ("XS", "XS")
+        S = ("S", "S")
+        M = ("M", "M")
+        L = ("L", "L")
+        XL = ("XL", "XL")
+        XXL = ("XXL", "XXL")
 
         @classmethod
         def get_value(cls, member):
             return cls[member].value
 
-    name = models.CharField()
-    price = models.DecimalField()
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     url = models.URLField()
-    description = models.CharField()
+    description = models.CharField(max_length=200)
     rating = models.DecimalField(max_digits=3, decimal_places=2)
     reviews_count = models.PositiveIntegerField()
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
@@ -51,4 +59,27 @@ class Product(models.Model):
         return self.name
 
     class Meta:
-        ordering = ["headline"]
+        ordering = ["date_added", "name"]
+
+
+class Basket(DateAddedMixin, models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cost = models.DecimalField(max_digits=15, decimal_places=2)
+    products = models.ManyToManyField(Product, through="ProductsAmount")
+
+    class Meta:
+        ordering = ["user"]
+
+    def __str__(self):
+        return self.user
+
+
+class ProductsAmount(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    basket = models.ForeignKey(Basket, on_delete=models.SET_NULL, null=True)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.product
--- ewardrobe/processing_data/merge_data_sources.py
+++ ewardrobe/processing_data/merge_data_sources.py
@@ -1,7 +1,7 @@
 import pandas
 import random
 from pathlib import Path
-from constance import COLUMNS
+from constants import COLUMNS
 
 
 def get_filenames() -> list:
@@ -13,7 +13,11 @@ def get_filenames() -> list:
 def merge_files(filenames: list) -> pandas.DataFrame:
     data = pandas.DataFrame(columns=COLUMNS)
     for file in filenames:
-        data = data.append(pandas.read_csv(file, header=0))
+        temp_data = pandas.read_csv(file, header=0)
+        for idx, value in enumerate(temp_data["price"]):
+            new_price = float(temp_data["price"].iloc[idx][1:])
+            temp_data["price"].iloc[idx] = new_price
+        data = data.append(temp_data)
     data = data.drop(
         ["mrp", "style_attributes", "total_sizes", "available_size"], axis=1
     )
@@ -23,4 +27,4 @@ def merge_files(filenames: list) -> pandas.DataFrame:
 if __name__ == "__main__":
     filenames = get_filenames()
     data = merge_files(filenames)
-    print(data.columns)
+    print(data)
