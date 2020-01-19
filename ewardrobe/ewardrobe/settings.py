67ea7731b8faeb0b41a5667f7bf431028d18e559
Ewelina Szmielew <ewelina@szmielew.pl>
2020-01-19 19:39:59 +0100

    admin panel part1

--- ewardrobe/ewardrobe/settings.py
+++ ewardrobe/ewardrobe/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "django.contrib.admindocs",
     "ewardrobe_app",
     "processing_data",
 ]
