1dbab5e0d9784c6745976cad4b7721f870161ff1
Ewelina Szmielew <ewelina@szmielew.pl>
2020-01-26 23:07:18 +0100

    Searching module for products finished

--- ewardrobe/ewardrobe_app/views/products.py
+++ ewardrobe/ewardrobe_app/views/products.py
@@ -11,6 +11,11 @@ class ProductsView(View):
     paginate_by = 20
 
     def get(self, request, *args, **kwargs):
+        if request.GET.get("q"):
+            return self.__search(request, *args, **kwargs)
+        return self.__paginated_products(request, *args, **kwargs)
+
+    def __paginated_products(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             products = Product.objects.all()
             paginator = Paginator(
@@ -18,6 +23,32 @@ class ProductsView(View):
             )  # Show 20 products per page.
             page_number = request.GET.get("page", 1)
             page_obj = paginator.get_page(page_number)
-            return render(request, self.template_name, {"page_obj": page_obj})
+            links = self.__construct_links(page_obj)
+            return render(
+                request, self.template_name, {"page_obj": page_obj, "links": links}
+            )
         else:
             return redirect(reverse(self.failure_url))
+
+    def __search(self, request, *args, **kwargs):
+        q = request.GET.get("q")
+        products = Product.objects.filter(name__icontains=q)
+        paginator = Paginator(products, self.paginate_by)  # Show 20 products per page.
+        page_number = request.GET.get("page", 1)
+        page_obj = paginator.get_page(page_number)
+        links = self.__construct_links(page_obj, q)
+        return render(
+            request, self.template_name, {"page_obj": page_obj, "links": links}
+        )
+
+    def __construct_links(self, page_obj, q=None):
+        output = dict()
+        if page_obj.has_previous():
+            output["previous"] = f"?page={page_obj.previous_page_number()}"
+        if page_obj.has_next():
+            output["next"] = f"?page={page_obj.next_page_number()}"
+        if q:
+            for key in output.keys():
+                output[key] += f"&q={q}"
+        return output
+
