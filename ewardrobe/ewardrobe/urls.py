b0c8797711b06950f4c61537d66772f78ea96b20
Ewelina Szmielew <ewelina@szmielew.pl>
2020-02-03 23:42:19 +0100

    Add forst part of single basket

--- ewardrobe/ewardrobe/urls.py
+++ ewardrobe/ewardrobe/urls.py
@@ -21,9 +21,10 @@ from ewardrobe_app.views.welcome import WelcomeView
 from ewardrobe_app.views.register import UserRegisterView
 from ewardrobe_app.views.login import UserLoginView
 from ewardrobe_app.views.logout import UserLogoutView
-from ewardrobe_app.views.products import ProductsView
-from ewardrobe_app.views.orders import OrdersView
 from ewardrobe_app.views.product import ProductView
+from ewardrobe_app.views.products import ProductsView
+from ewardrobe_app.views.basket import BasketView
+from ewardrobe_app.views.baskets import BasketsView
 
 
 urlpatterns = [
@@ -36,5 +37,6 @@ urlpatterns = [
     path("main/", MainView.as_view(), name="main"),
     path("products/<int:id>", ProductView.as_view(), name="product"),
     path("products/", ProductsView.as_view(), name="products"),
-    path("orders/", OrdersView.as_view(), name="orders"),
+    path("basket/", BasketView.as_view(), name="basket"),
+    path("baskets/", BasketsView.as_view(), name="baskets"),
 ]
