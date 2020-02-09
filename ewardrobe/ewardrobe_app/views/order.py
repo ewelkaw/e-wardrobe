4bf1c8e84d9ffd20266370a5774993d9ec5ef5ac
Ewelina Szmielew <ewelina@szmielew.pl>
2020-02-09 20:23:15 +0100

    Make order reorganization and form validation fix

--- ewardrobe/ewardrobe_app/views/order.py
+++ ewardrobe/ewardrobe_app/views/order.py
@@ -10,6 +10,7 @@ class OrderView(View):
     failure_url = "product"
 
     def post(self, request):
+
         if request.user.is_authenticated:
             product_id = request.POST.get("product_id")
             amount = request.POST.get("amount")
@@ -17,13 +18,15 @@ class OrderView(View):
 
             product = Product.objects.get(id=product_id)
             basket, _ = Basket.objects.get_or_create(status=0, user=request.user)
-            ProductsAmount.objects.create(
-                basket=basket, product=product, size=size, amount=amount
+            basket.save()
+
+            product_amount, _ = ProductsAmount.objects.get_or_create(
+                basket=basket, product=product, size=size
             )
-            # products_amounts = ProductsAmount.objects.filter(basket=basket).all()
-            products_amounts = ProductsAmount.objects.values("product").annotate(
-                Sum("amount")
-            )
+            product_amount.amount += int(amount)
+            product_amount.save()
+
+            products_amounts = ProductsAmount.objects.filter(basket=basket).all()
 
             return render(
                 request,
