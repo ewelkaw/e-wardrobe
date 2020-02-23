659f1754d245621ac66bed412ce1f1369dbb73bb
Ewelina Szmielew <ewelina@szmielew.pl>
2020-02-24 00:04:45 +0100

    Add payments with stripe

--- ewardrobe/ewardrobe_app/views/orders.py
+++ ewardrobe/ewardrobe_app/views/orders.py
@@ -1,8 +1,16 @@
 from django.views.generic import View
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
+
+import stripe
+
 from ewardrobe_app.models import Basket, ProductsAmount
 from ewardrobe_app.queries.orders import OrderWorkflow
+from ewardrobe_app.queries.basket import BasketWorkflow
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class OrdersView(View):
@@ -13,9 +21,15 @@ class OrdersView(View):
         if request.user.is_authenticated:
             basket_id = request.POST.get("basket")
             basket = Basket.objects.get(user=request.user, id=basket_id)
-
+            prepared_basket = BasketWorkflow(user=request.user).get_current_basket()
             if request.POST.get("pay"):
-                if ProductsAmount.objects.filter(basket=basket).all():
+                if ProductsAmount.objects.filter(basket=basket).exists():
+                    stripe.Charge.create(
+                        amount=prepared_basket["total_cost_in_cents"],
+                        currency="usd",
+                        description="A Django charge",
+                        source=request.POST["stripeToken"],
+                    )
                     basket.pay()
 
             if request.POST.get("cancel"):
