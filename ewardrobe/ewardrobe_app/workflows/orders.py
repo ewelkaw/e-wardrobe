aa4fff5c2c23b8731bd1a82403f41bbfd22bb5bb
Ewelina Szmielew <ewelina@szmielew.pl>
2020-03-04 00:24:50 +0100

    Filtering buttons for user actions according to orders

--- ewardrobe/ewardrobe_app/workflows/orders.py
+++ ewardrobe/ewardrobe_app/workflows/orders.py
@@ -1,6 +1,6 @@
 from ewardrobe_app.models import Basket
 from django.contrib.auth.models import User
-from ewardrobe_app.models import STATUS_CHOICES, STATUS_OPENED
+from ewardrobe_app.models import STATUS_CHOICES, STATUS_OPENED, STATUS_RETURNED
 
 
 class OrderWorkflow:
@@ -12,7 +12,7 @@ class OrderWorkflow:
         for idx, order in enumerate(
             Basket.objects.filter(user=self.__user)
             .exclude(status=STATUS_OPENED)
-            .order_by("-date_created")
+            .order_by("-date_modified")
         ):
 
             orders_data.append(
@@ -29,5 +29,6 @@ class OrderWorkflow:
         return [
             transition.name.capitalize()
             for transition in list(order.get_available_status_transitions())
+            if transition.target in [STATUS_RETURNED]
         ]
 
