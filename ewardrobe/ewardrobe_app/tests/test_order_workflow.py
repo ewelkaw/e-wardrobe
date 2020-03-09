19ffabc54bb7268c627b9cea6117bca703a241db
Ewelina Szmielew <ewelina@szmielew.pl>
2020-03-09 20:22:32 +0100

    fix for orders workflow test

--- ewardrobe/ewardrobe_app/tests/test_order_workflow.py
+++ ewardrobe/ewardrobe_app/tests/test_order_workflow.py
@@ -19,59 +19,39 @@ class OrderWorkflowTestCase(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        date = datetime.date.today() - datetime.timedelta(days=15)
         cls.product = ProductFactory()
         cls.user1 = UserFactory()
         cls.user2 = UserFactory()
-        for idx, status in enumerate(
-            [
-                STATUS_OPENED,
-                STATUS_PAID,
-                STATUS_SHIPPED,
-                STATUS_SHIPPED,
-                STATUS_CLOSED,
-                STATUS_RETURNED,
-            ]
-        ):
-            if status == STATUS_SHIPPED and idx == 2:
-                date = datetime.date.today() - datetime.timedelta(days=15)
-                cls.basket = Basket.objects.create(
-                    user=cls.user2, status=status, date_modified=date,
-                )
-                print("1", cls.basket.date_modified)
-            if status == STATUS_SHIPPED and idx == 3:
-                cls.basket = Basket.objects.create(user=cls.user2, status=status)
-                print("2", cls.basket.date_modified)
-            else:
-                cls.basket = Basket.objects.create(user=cls.user1, status=status)
-                print("3", cls.basket.date_modified)
+        for status in [
+            STATUS_OPENED,
+            STATUS_PAID,
+            STATUS_SHIPPED,
+            STATUS_CLOSED,
+            STATUS_RETURNED,
+        ]:
+            cls.basket = Basket.objects.create(user=cls.user1, status=status)
+            Basket.objects.filter(id=cls.basket.id).update(date_modified=date)
             cls.products_amount = ProductsAmount(
                 basket=cls.basket, product=cls.product, size="S"
             )
-            # orders[0]['order'].date_modified
 
-        orders = OrderWorkflow(user=cls.user2).prepare_baskets()
-        import ipdb
-
-        ipdb.set_trace()
+        # setup one shipped basket with 14 days return period
+        cls.basket = Basket.objects.create(user=cls.user2, status=STATUS_SHIPPED)
 
     def test_orders_non_return_status_workflow(self):
         orders = OrderWorkflow(user=self.user1).prepare_baskets()
         for order in orders:
             assert isinstance(order["order"], Basket)
-            assert order["status_name"] in ["Paid", "Closed", "Returned"]
+            assert order["status_name"] in ["Paid", "Closed", "Returned", "Shipped"]
             assert not order["return_status"]
-        assert len(orders) == 3
+        assert len(orders) == 4
 
     def test_orders_return_status_workflow(self):
         orders = OrderWorkflow(user=self.user2).prepare_baskets()
-        for order in orders:
-            assert isinstance(order["order"], Basket)
-            assert order["status_name"] in ["Shipped"]
-            # assert order["return_status"]
-            assert (
-                order["return_status"]
-                if order["order"].date_modified == datetime.date.today()
-                else not order["return_status"]
-            )
-        assert len(orders) == 2
+        print("orders", orders)
+        assert isinstance(orders[0]["order"], Basket)
+        assert orders[0]["status_name"] in ["Shipped"]
+        assert orders[0]["return_status"]
+        assert len(orders) == 1
 
